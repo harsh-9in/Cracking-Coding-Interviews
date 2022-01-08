e0292aba88ca30a83d6a021cfef08edaca8e59a2
harsh-9in <harshkumar9.in@gmail.com>
2022-01-08 16:37:57 +0530

    Hoiesting in js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//  Hoiesting in js
+
+// This is the vesy first concept in js
+//  accoring to this concept we get to know about 
+// declaration in js.
+//  how a variable  and function is used before defining 
+// for this we need to remember the execution context of js.
+
+
+console.log(x);
+hello();
+console.log(hello);
+
+
+var x=70;
+function hello(){
+    console.log("Hello Js")
+}
+
