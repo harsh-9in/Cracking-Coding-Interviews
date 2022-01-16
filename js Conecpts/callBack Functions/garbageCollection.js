d143b2f9f323de0793f205877801ffc456d67dff
harsh-9in <harshkumar9.in@gmail.com>
2022-01-16 13:14:58 +0530

    garbage Collection

---
+++ js Conecpts/callBack Functions/garbageCollection.js
@@ -0,0 +1,4 @@
+//  We generally dont use much event listeners to our website 
+//  because it forms closure and take its own lexical scope 
+//  to execute it differently 
+// which  makees the webpage heave and consumes a lot of memory.
